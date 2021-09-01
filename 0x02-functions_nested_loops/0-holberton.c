41ad6fc1c931503ab6f713d98c47d7b7228851d5
Edith45 <edithpurplet@gmail.com>
2021-09-01 11:35:58 -0700

    putcahr

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,5 +1,17 @@
-#include <stdio.h>
+#include "main.h"
 /* This program prints putchar*/
 
-int putchar(int 0);
-Return 0;
+int main (void)
+{
+int _putchar(int 0);
+        _putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
